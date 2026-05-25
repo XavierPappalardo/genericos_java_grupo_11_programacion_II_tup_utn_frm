ffc1c55ad3478bfa4bd6ef10a754b684c7b356ff
XavierPappalardo <xavierdampapp@gmail.com>
2026-05-25 18:20:18 -0300

    Ajustes al Ejercicio 3 y al Main
    Añado excepciones y validaciones de tipos y comentarios al código para una estructura clara

--- GenericosJavaGrupo11/src/Ejercicio_03/ParNumeros.java
+++ GenericosJavaGrupo11/src/Ejercicio_03/ParNumeros.java
@@ -1,15 +1,16 @@
 package Ejercicio_03;
 
+//Clase genérica ParNumeros
+
 public class ParNumeros<A,B> {
+
+    //Atributos (Los números del par)
 
     private A numero1;
 
     private B numero2;
 
-    public ParNumeros(A numero1, B numero2) {
-        this.numero1 = numero1;
-        this.numero2 = numero2;
-    }
+    //Getters
 
     public A getNumero1() {
         return numero1;
@@ -19,23 +20,70 @@ public class ParNumeros<A,B> {
         return numero2;
     }
 
+    //Setters
+
+    public void setNumero1(A numero1) {
+
+        //Condición de que sea un Integer para que sea setteado
+
+        if (numero1 instanceof Integer){
+
+            this.numero1 = numero1;
+
+        } else{
+
+            //Excepción en caso de que no cumpla la condición
+
+            throw new IllegalArgumentException("El dato ingresado debe ser un número entero.");
+
+        }
+    }
+
+    public void setNumero2(B numero2) {
+
+        //Condición de que sea un Integer para que sea setteado
+
+        if (numero2 instanceof Integer){
+
+            this.numero2 = numero2;
+
+        } else{
+
+            //Excepción en caso de que no cumpla la condición
+
+            throw new IllegalArgumentException("El dato ingresado debe ser un número entero.");
+
+        }
+    }
+
+    //Método mostrarPar
+
     public void mostrarPar(A numero1, B numero2) {
 
+        //Condición de que el usuario haya ingresado ambos números del par para mostrarlo
+
         if (this.numero1 != null && this.numero2 != null) {
+
+            //Muestra el par ordenado
 
             System.out.println("-------PAR DE NÚMEROS--------");
             System.out.println("Número 1: " + this.numero1 + "  |  Número 2: " + this.numero2 + "\n");
 
         } else {
 
-            System.out.println("No se pueden invertir los números si el par está incompleto.\n");
+            //Excepción en caso de que no cumpla la condición
 
+            throw new IllegalArgumentException("El par debe estar completo para mostrarlo");
 
         }
 
     }
 
+    //Método invertir
+
     public void invertir(A numero1, B numero2){
+
+        //Condición de que el usuario haya ingresado ambos números del par para invertirlo
 
         if (this.numero1 != null && this.numero2 != null){
 
@@ -45,7 +93,9 @@ public class ParNumeros<A,B> {
 
         } else {
 
-            System.out.println("No se pueden invertir los números si el par está incompleto.\n");
+            //Excepción en caso de que no cumpla la condición
+
+            throw new IllegalArgumentException("El par debe estar completo para mostrarlo");
 
         }
 
--- GenericosJavaGrupo11/src/Main.java
+++ GenericosJavaGrupo11/src/Main.java
@@ -1,16 +1,36 @@
 import Ejercicio_03.*;
 
+//Clase Main del proyecto
+
 public class Main {
+
+    //Método main
 
     public static void main(String[] args){
 
-        ParNumeros<Integer, Integer> parNumeros = new ParNumeros(22, 44);
+        //Ejercicio 3
 
-        parNumeros.mostrarPar(null, null);
+        //Instanciamos un nuevo par
 
-        parNumeros.invertir(null, null);
+        ParNumeros<Integer, Integer> parNumeros = new ParNumeros();
 
-        parNumeros.mostrarPar(null, null);
+        //Setteamos ambos números
+
+        parNumeros.setNumero1(33);
+
+        parNumeros.setNumero2(22);
+
+        //Mostramos el par ordenado
+
+        parNumeros.mostrarPar(parNumeros.getNumero1(), parNumeros.getNumero2());
+
+        //Invertimos el par
+
+        parNumeros.invertir(parNumeros.getNumero1(), parNumeros.getNumero2());
+
+        //Mostramos el par con el nuevo orden
+
+        parNumeros.mostrarPar(parNumeros.getNumero1(), parNumeros.getNumero2());
 
     }
 
